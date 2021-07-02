a4ea565eaedc55c1e63a0da08a12db037344d2e9
Berkay Kulak <beko_468@hotmail.com>
2021-07-02 13:56:57 +0300

    2/07

--- IdentitySystem/Controllers/MemberController.cs
+++ IdentitySystem/Controllers/MemberController.cs
@@ -1,16 +1,48 @@
-﻿using Microsoft.AspNetCore.Authorization;
+﻿using IdentitySystem.Models;
+using IdentitySystem.ViewModels;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-
+using Mapster;
 namespace IdentitySystem.Controllers
 {
     [Authorize]// membercontrollere sadece üyeler erişecek.
     public class MemberController : Controller
     {
+        public UserManager<AppUser> userManager { get; }
+        public SignInManager<AppUser> signInManager { get; }
+        public MemberController(UserManager<AppUser> userManager, SignInManager<AppUser> signInManager)
+        {
+            this.userManager = userManager;
+            this.signInManager = signInManager;
+        }
+
+
         public IActionResult Index()
+        {
+            // kullanıcı bir siteye geldiği zaman, üye de olsa olmasada user classı oluşturur
+            // bir tane kimliği oluşur, eğer kullanıcı giriş yapmamışsa isauthenticate false olur.
+            // boş bir kimlik oluşur. login olursa biz name,usernama gibi alanları yakalayabiliriz.
+            //User.Identity.
+
+
+            AppUser user = userManager.FindByNameAsync(User.Identity.Name).Result;
+            // userin içindeki propertylerden UserViewModel içerisindeki Propertyler ile eşleşenleri
+            // userViewModel'e aktaracak
+            UserViewModel userViewModel = user.Adapt<UserViewModel>();
+
+            //UserViewModel userViewModel = new UserViewModel();
+            //userViewModel.UserName = user.UserName;
+
+
+            return View(userViewModel);
+        }
+
+        public IActionResult PasswordChange()
         {
             return View();
         }
---
+++ IdentitySystem/ViewModels/PasswordChangeViewModel.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace IdentitySystem.ViewModels
+{
+    public class PasswordChangeViewModel
+    {
+        [Display(Name ="Eski Şifreniz")]
+        [Required(ErrorMessage ="Eski şifeniz gereklidir.")]
+        [DataType(DataType.Password)]
+        [MinLength(4,ErrorMessage ="Şifreniz en az 4 karakterli olmak zorundadır.")]
+        public string PasswordOld { get; set; }
+
+        [Display(Name = "Yeni Şifreniz")]
+        [Required(ErrorMessage = "Yeni şifeniz gereklidir.")]
+        [DataType(DataType.Password)]
+        [MinLength(4, ErrorMessage = "Şifreniz en az 4 karakterli olmak zorundadır.")]
+       
+        public string PasswordNew { get; set; }
+
+        [Display(Name = "Onay Yeni Şifreniz")]
+        [Required(ErrorMessage = "Onay yeni şifre  gereklidir.")]
+        [DataType(DataType.Password)]
+        [MinLength(4, ErrorMessage = "Şifreniz en az 4 karakterli olmak zorundadır.")]
+        [Compare("PasswordNew",ErrorMessage ="Yeni şifreniz ve onay şifreniz birbirinden farklıdır.")]
+        public string PasswordConfirm { get; set; }
+
+
+    }
+}
